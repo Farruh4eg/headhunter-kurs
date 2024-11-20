baf54c64df852f1071a0fc167d9743709c9662ac
farruh4eg <farruh4eg@gmail.com>
2024-11-20 07:43:27 +0300

    fix job page (hopefully)

---
+++ prisma/migrations/20241120044128_test/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "joblistings" ALTER COLUMN "experience" SET DEFAULT 0;
--- src/lib/components/Job.svelte
+++ src/lib/components/Job.svelte
@@ -1,90 +1,80 @@
 <script lang="ts">
-  import {
-    addSpaceInString,
-  } from '$lib/utils/helpers';
-  import ButtonLike from './ButtonLike.svelte';
-  import ButtonApply from './ButtonApply.svelte';
-  import { session } from '$lib/session';
-  import { onMount } from 'svelte';
+	import { addSpaceInString } from '$lib/utils/helpers';
+	import ButtonLike from './ButtonLike.svelte';
+	import ButtonApply from './ButtonApply.svelte';
+	import { session } from '$lib/session';
+	import { onMount } from 'svelte';
 
-  let {job, user_id} = $props()
+	let { job, user_id } = $props();
 
-  let isLoggedIn: boolean = $state($session.isLoggedIn);
-  let notice = '';
+	let isLoggedIn: boolean = $state($session.isLoggedIn);
 
-  let job_id = job.job_id;
-  let jobTitle = job.title;
+	let job_id = job.job_id;
+	let jobTitle = job.title;
 	let jobSalary = addSpaceInString(job.salary.toString());
 	let jobEmployerCompany = job.employers.company;
 	let jobEmployerLogo = job.employers.logo;
-  let jobExperience = job.experience;
-  let jobDescription = job.description;
+	let jobExperience = job.experience;
+	let jobDescription = job.description;
 </script>
 
 <svelte:head>
-  <title>
-    {jobTitle}
-  </title>
+	<title>
+		{jobTitle}
+	</title>
 </svelte:head>
 
 <section
-  class="flex w-[1280px] my-0 mx-auto box-border p-8 text-gray-700 flex-col gap-y-10 items-end"
+	class="mx-auto my-0 box-border flex w-[1280px] flex-col items-end gap-y-10 p-8 text-gray-700"
 >
-  <section class="w-full bg-white p-6 text-gray-700 rounded-xl">
-    <section class="flex">
-      <section class="flex flex-col w-[600px] px-10 gap-y-4">
-        <section class="flex w-full h-24 items-start justify-start">
-          <section class="flex w-full">
-            <h1 class="pb-4 flex items-end gap-x-4 flex-wrap overflow-hidden">
-              <section
-                class="font-bold text-3xl w-max flex flex-wrap text-wrap"
-              >
-                <span class="flex text-wrap flex-wrap">
-                  {jobTitle}
-                </span>
-              </section>
-              <a
-                class="no-underline text-blue-600 hover:cursor-pointer"
-                on:click={() =>
-                  document.getElementById('specs')?.scrollIntoView({
-                    behavior: 'smooth',
-                  })}>подробнее</a
-              >
-            </h1>
-          </section>
-        </section>
-        <section class="mt-4 w-full rounded-lg flex gap-x-4">
-          <section
-            class="flex items-end gap-x-1 w-full py-4 pl-4 pr-8 bg-gray-100 bg-opacity-80 rounded-lg"
-          >
-            <span class="font-semibold text-2xl text-blue-600"
-              >{jobSalary} &#8381;</span
-            >
-          </section>
-          {#if isLoggedIn}
-            <section class="flex gap-x-4 items-end">
-              <ButtonLike {user_id} {job_id} />
-              <ButtonApply {user_id} {job_id} />
-            </section>
-          {/if}
-        </section>
-        
-        <section class="flex justify-start mx-1">
-          <a href="/search/jobs?q=&company={jobEmployerCompany}">
-            <img
-              src="/companies/{jobEmployerLogo}"
-              alt="employer-logo"
-              class="h-full"
-            />
-          </a>
-        </section>
-      </section>
-    </section>
-  </section>
-  <section
-    class="flex flex-col w-2/3 bg-white rounded-lg p-4 gap-y-4 mx-auto"
-    id="specs"
-  >
-    <p>{jobDescription}</p>
-  </section>
+	<section class="w-full rounded-xl bg-white p-6 text-gray-700">
+		<section class="flex justify-center items-start">
+			<section class="flex w-[800px] flex-col gap-y-4 shadow-xl rounded-lg p-4 px-10 min-h-[60vh]">
+				<section class="flex h-24 w-full items-start justify-start">
+					<section class="flex w-full justify-between">
+						<h1 class="flex flex-wrap items-end gap-x-4 overflow-hidden pb-4">
+							<section class="flex w-max flex-wrap text-wrap text-3xl font-bold">
+								<span class="flex flex-wrap text-wrap">
+									{jobTitle}
+								</span>
+							</section>
+						</h1>
+						<section class="mx-1 flex h-24 justify-start">
+							<a href="/search/jobs?q=&company={jobEmployerCompany}">
+								<img src="/companies/{jobEmployerLogo}" alt="employer-logo" class="h-full" />
+							</a>
+						</section>
+					</section>
+				</section>
+				<section class="flex w-full gap-x-4 rounded-lg">
+					<section
+						class="flex w-full items-center gap-x-4 rounded-lg py-4 pl-4 pr-8 font-semibold shadow-md"
+					>
+						<span>Заработная плата: от </span>
+						<span class="text-2xl text-blue-600">{jobSalary} &#8381;</span>
+					</section>
+					{#if isLoggedIn}
+						<section class="flex items-end gap-x-4">
+							<ButtonLike {user_id} {job_id} />
+							<ButtonApply {user_id} {job_id} />
+						</section>
+					{/if}
+				</section>
+				<section
+						class="flex w-full items-center gap-x-4 rounded-lg py-4 pl-4 pr-8 font-semibold shadow-md"
+					>
+						<span>Требуемый опыт работы (в годах): </span>
+						{#if jobExperience}
+						<span>{jobExperience}</span>
+						{:else}
+						<span>не требуется</span>
+						{/if}
+					</section>
+				<section class="flex w-2/3 flex-col gap-y-4 rounded-lg p-4" id="description">
+					<h1 class="text-2xl font-bold">Описание вакансии</h1>
+					<p class="text-lg">{jobDescription}</p>
+				</section>
+			</section>
+		</section>
+	</section>
 </section>
